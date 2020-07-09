f30a00f6a32c7b81ec5a16503a618eb8bfd1a255
Joffre Ramirez <joffreramirez.r@hotmail.com>
2020-07-09 12:07:13 -0500

    Codigo en java

---
+++ SistemaClinico_DC/src/sistemaclinico_dc/Persona.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemaclinico_dc;
+
+import java.util.Date;
+
+/**
+ *
+ * @author User
+ */
+public class Persona {
+    protected String Usuario;
+    protected String clave;
+    protected String nombre;
+    protected String apellido;
+    protected String cedula;
+    protected String direccion;
+    protected Date fecha;
+    
+    
+    public boolean login(){
+        
+    }
+    
+    public boolean logout(){
+        
+    }
+}
